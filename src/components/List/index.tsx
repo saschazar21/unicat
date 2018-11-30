48bc082d0b13529adde0933e63fa3bdee03d1f1e
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2018-11-30 16:34:57 +0100

    feat(List): Added List component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,72 @@
+import React, { Component, ReactChild } from 'react';
+import { style } from 'typestyle';
+
+export interface IListProps {
+  /** A list consists of children, so the <List> tag should always act as wrapper */
+  readonly children: ReactChild[];
+  /** Render the list horizontally or vertically */
+  readonly horizontal?: boolean;
+  /** Whether to render a dash as list indicator */
+  readonly indicator?: boolean;
+}
+
+export default class List extends Component<IListProps> {
+  /**
+   * The ID base string, will be used as prefix for the key="" attributes
+   */
+  private ID: string = '';
+
+  /**
+   * The ID factory, either returns the already randomly generated ID prefix, or creates a new one
+   */
+  get id(): string {
+    if (this.ID.length < 1) {
+      const base = 'abcdef0123456789';
+      const arr = [0, 1, 2, 3, 4].map((n: number) => base.charAt(Math.floor(Math.random() * base.length)));
+      this.ID = arr.join('');
+    }
+    return this.ID;
+  }
+
+  /**
+   * The CSS styles for the list
+   */
+  get list(): string {
+    const { indicator, horizontal } = this.props;
+    return style({
+      $nest: {
+        '& > li': {
+          display: 'list-item',
+          padding: '.5rem',
+          width: horizontal ? 'auto' : '100%',
+        },
+        '& > li::before': {
+          content: indicator && !horizontal ? `'-'` : ``,
+          marginLeft: '-1.25rem',
+          marginRight: '.75rem',
+        },
+      },
+      alignItems: indicator && !horizontal ? 'flex-start' : 'center',
+      display: 'flex',
+      flexDirection: horizontal ? 'row' : 'column',
+      flexWrap: horizontal ? 'wrap' : 'nowrap',
+      justifyContent: horizontal ? 'center' : 'flex-start',
+      listStyleType: 'none',
+      margin: 0,
+      padding: 0,
+      paddingLeft: indicator && !horizontal ? '.75rem' : 0,
+    });
+  }
+
+  /**
+   * The main render function
+   */
+  public render(): JSX.Element {
+    const { children} = this.props;
+    return (
+      <ul className={this.list}>
+        {children.map((child: ReactChild, index: number) => <li key={`${this.id}-${index}`}>{child}</li>)}
+      </ul>
+    );
+  }
+}
