b34461a510408730a472c0bbf42be3ab9a7fe1da
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-05-24 07:48:05 +0200

    refactor(scss): refactored TypeScript definitions to SCSS

--- src/__styles__/borders.ts
+++
@@ -1,8 +0,0 @@
-import { Variant } from '../__data__/definitions';
-import { colors } from './colors';
-
-export const borderRadius = ['4px', '9999px'];
-
-export const borderWidths = ['0.5px', '2px'];
-
-export const borderColor = colors[Variant.Light].hex;
--- src/__styles__/index.ts
+++
@@ -1,5 +0,0 @@
-import { DefaultTheme } from 'styled-components';
-
-export const UnicatTheme: DefaultTheme = {};
-
-export { UnicatTheme as default };
