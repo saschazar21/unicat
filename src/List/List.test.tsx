7e2972e94fe234d45da2752a6680bb63ef1a013c
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-06-02 12:54:10 +0200

    test(List): enhanced tests for List

--- src/List/List.test.tsx
+++ src/List/List.test.tsx
@@ -3,12 +3,35 @@ import { shallow } from 'enzyme';
 
 import List from './List';
 
+const items = ['Obi-Wan Kenobi', 'Luke Skywalker', 'Yoda'];
+
 describe('List', () => {
   it('renders', () => {
-    const items = ['Obi-Wan Kenobi', 'Luke Skywalker', 'Yoda'];
     const wrapper = shallow(<List items={items} />);
 
     expect(wrapper.find('span')).toHaveLength(items.length);
     expect(wrapper.find('li')).toHaveLength(items.length);
+  });
+
+  it('renders a horizontal list', () => {
+    const wrapper = shallow(<List items={items} horizontal />);
+
+    expect(wrapper.find('.horizontal')).toHaveLength(1);
+  });
+
+  it('renders an ordered list', () => {
+    const wrapper = shallow(<List items={items} ordered />);
+
+    expect(wrapper.find('ol')).toHaveLength(1);
+    expect(wrapper.find('ul')).toHaveLength(0);
+  });
+
+  it('renders a list with HTML items', () => {
+    const customItems = items.map(i => <strong>{i}</strong>);
+
+    const wrapper = shallow(<List items={customItems} />);
+
+    expect(wrapper.find('strong')).toHaveLength(customItems.length);
+    expect(wrapper.find('span')).toHaveLength(0);
   });
 });
