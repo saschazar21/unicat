9fe23fbae1d64e0fb1ef09bc246543ac93699a85
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-06-14 16:57:28 +0200

    feat(IconButton): added IconButton component

---
+++ src/IconButton/IconButton.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+import Button, { ButtonProps } from '../Button/Button';
+
+import styles from './IconButton.scss';
+
+export interface IconButtonProps extends ButtonProps {
+  icon: JSX.Element;
+}
+
+export default class IconButton extends Component<IconButtonProps> {
+  public render() {
+    const { variant: defaultVariant } = Button.defaultProps;
+    const {
+      block,
+      disabled,
+      icon,
+      className: customClassName,
+      large,
+      title,
+      variant = defaultVariant,
+      ...props
+    } = this.props;
+
+    const className = classnames(
+      styles.wrapper,
+      { [styles[variant]]: !disabled },
+      { [styles.large]: large, [styles.disabled]: disabled, [styles.block]: block },
+      customClassName,
+    );
+
+    return (
+      <button
+        className={className}
+        {...props}
+        disabled={!!disabled}
+        value={title}
+      >
+        {icon}
+      </button>
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Heading } from './Heading';
+export { default as IconButton } from './IconButton';
 export { default as List } from './List';
 export { default as Modal } from './Modal';
 export { default as Picture } from './Picture';
