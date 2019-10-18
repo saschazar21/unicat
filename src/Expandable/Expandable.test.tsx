abed2c122f77a9f62ba70436bbf09bbf964f381a
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-10-18 21:29:32 +0200

    refactor(Expandable): refactored Expandable component

--- src/Expandable/Expandable.test.tsx
+++ src/Expandable/Expandable.test.tsx
@@ -14,4 +14,31 @@ describe('Expandable', () => {
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
+
+  it('has the .open class', () => {
+    const wrapper = shallow(
+      <Expandable title="Test" open>
+        <p>Test Body</p>
+      </Expandable>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('.open')).toHaveLength(1);
+  });
+
+  it('calls handleClick on click', () => {
+    const wrapper = shallow(
+      <Expandable title="Test">
+        <p>Test Body</p>
+      </Expandable>
+    );
+    const button = wrapper.find('.activator');
+
+    expect(button).toHaveLength(1);
+    expect(wrapper.find('.open')).toHaveLength(0);
+
+    button.simulate('click');
+
+    expect(wrapper.find('.open')).toHaveLength(1);
+  });
 });
--- src/Expandable/Expandable.tsx
+++ src/Expandable/Expandable.tsx
@@ -1,16 +1,9 @@
-import React, {
-  ReactNode,
-  SyntheticEvent,
-  useEffect,
-  useRef,
-  useState,
-  MutableRefObject,
-} from 'react';
+import React, { useState, ReactNode, SyntheticEvent } from 'react';
 import classnames from 'classnames';
 import { ChevronIcon } from '@saschazar/unicat-icons';
+import AnimateHeight from 'react-animate-height';
 
 import Heading from '../Heading';
-import { animationFrame } from '../__tools__/hooks';
 import { preventDefault } from '../__tools__/helpers';
 
 import styles from './Expandable.scss';
@@ -31,19 +24,6 @@ export interface ExpandableProps {
 export default function expandable(props: ExpandableProps) {
   const { children, className: customClassname, open: isOpen, title } = props;
   const [open, setOpen] = useState(!!isOpen);
-  const [height, setHeight] = useState(0);
-  const ref = useRef(0);
-  const contentRef = useRef<HTMLElement>();
-
-  const { progress, animate } = animationFrame(ANIMATION_DEFAULT_DURATION, () =>
-    setOpen(o => !o)
-  );
-
-  useEffect(() => {
-    const { current: { scrollHeight = 0 } = {} } = contentRef;
-    const percentage = open ? 1 - progress : progress;
-    setHeight(Math.round(scrollHeight * percentage));
-  }, [progress]);
 
   const className = classnames(
     { [styles.open]: open },
@@ -53,24 +33,23 @@ export default function expandable(props: ExpandableProps) {
 
   const handleClick = (event: SyntheticEvent) => {
     preventDefault(event);
-    ref.current = requestAnimationFrame(animate);
+    setOpen(o => !o);
   };
 
   return (
     <aside className={styles.wrapper}>
       <button className={className} onClick={handleClick}>
-        <Heading level="h4" className={styles.title}>
+        <Heading level="h5" className={styles.title}>
           {title}
         </Heading>
         <ChevronIcon className={styles.icon} />
       </button>
-      <section
-        className={styles.content}
-        style={{ maxHeight: `${height}px` }}
-        ref={contentRef as MutableRefObject<HTMLElement>}
+      <AnimateHeight
+        duration={ANIMATION_DEFAULT_DURATION}
+        height={open ? 'auto' : 0}
       >
-        {children}
-      </section>
+        <section className={styles.content}>{children}</section>
+      </AnimateHeight>
     </aside>
   );
 }
